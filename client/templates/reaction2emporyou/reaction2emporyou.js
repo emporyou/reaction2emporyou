14187999eafad31b32f812bb4c2255cc56c00291
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-21 03:59:22 +0200

    Update reaction2emporyou.js

--- client/templates/reaction2emporyou/reaction2emporyou.js
+++ client/templates/reaction2emporyou/reaction2emporyou.js
@@ -1,1 +1,7 @@
-c
+Template.reaction2emporyou.message = () => {
+  return "Hello World!";
+};
+
+Template.reaction2emporyou.description = () => {
+  return "An example package to use as a template for custom Reaction packages.";
+};
