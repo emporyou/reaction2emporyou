f2dfe31bb71ca2a414cc20a56c0dfd3e86dbc76c
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-21 02:57:41 +0200

    Update package.js

--- package.js
+++ package.js
@@ -32,14 +32,6 @@ Package.on_use(function (api) {
 
   api.addFiles("common/routing.js", ["client", "server"]); // common routing
 
-BrowserPolicy.content.allowConnectOrigin("https://*.emporyoum.com");
-BrowserPolicy.content.allowConnectOrigin("http://*.emporyoum.com");
-BrowserPolicy.content.allowConnectOrigin("https://emporyoum.com");
-BrowserPolicy.content.allowConnectOrigin("http://emporyoum.com");
-BrowserPolicy.content.allowConnectOrigin("https://*.emporyou.com");
-BrowserPolicy.content.allowConnectOrigin("http://*.emporyou.com");
-BrowserPolicy.content.allowConnectOrigin("https://emporyou.com");
-BrowserPolicy.content.allowConnectOrigin("http://emporyou.com");
 
   api.addFiles([
     "client/templates/emporyou-connector/emporyou-connector.html",
