f1d19a48ca3243b641d00f3150c5faa9c16e33e7
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-21 03:52:17 +0200

    Update emporyou-connector.js

--- client/templates/emporyou-connector/emporyou-connector.js
+++ client/templates/emporyou-connector/emporyou-connector.js
@@ -1,1 +1,8 @@
 /**/
+Template['emporyou-connector'].message = () => {
+  return "Hello World!";
+};
+
+Template['emporyou-connector'].description = () => {
+  return "An example package to use as a template for custom Reaction packages.";
+};
