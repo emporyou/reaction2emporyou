75a11a5a453e860643a1e8980027274328b55680
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-21 03:49:36 +0200

    Delete helloworld.js

--- client/templates/helloworld/helloworld.js
+++
@@ -1,7 +0,0 @@
-Template.helloworld.message = () => {
-  return "Hello World!";
-};
-
-Template.helloworld.description = () => {
-  return "An example package to use as a template for custom Reaction packages.";
-};
