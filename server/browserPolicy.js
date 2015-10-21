0cf1c6046bf946e80160587c5f90e8bfca725cee
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-21 04:09:14 +0200

    Update browserPolicy.js

--- server/browserPolicy.js
+++ server/browserPolicy.js
@@ -1,4 +1,5 @@
 Meteor.startup(function() {
   BrowserPolicy.content.allowEval();
   return BrowserPolicy.content.allowOriginForAll("http://emporyoum.com");
+  return BrowserPolicy.content.allowOriginForAll("http://emporyoum.com");
 });
