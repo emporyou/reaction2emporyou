6e8076a180ed507414f2aa9459ba7b4d83427992
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2015-10-31 18:42:11 +0100

    chenged name to emporyou:...

--- server/browserPolicy.js
+++ server/browserPolicy.js
@@ -3,7 +3,7 @@ Meteor.startup(function() {
 if(!b){b=browserPolicy;}
   if(b){
 b.content.allowEval();
-B.content.allowOriginForAll("http://emporyou.com");
+b.content.allowOriginForAll("http://emporyou.com");
 b.content.allowOriginForAll("http://*.emporyou.com");
   return b.content.allowOriginForAll("http://emporyoum.com");
 }});
